2e8971c793b773ca2513ccb55c35746b798ddac0
Alonso <alonso12_284@outlook.com>
2021-03-17 18:39:37 -0600

    setCode bugFixes

--- main.py
+++ main.py
@@ -57,9 +57,38 @@ async def getCode(ctx, modulo, dia):
 
 @bot.command()
 async def setCode(ctx, modulo, dia, codigo):
+
+    #modulo no existe
+    if(not(modulo in magIA)):
+        await ctx.send('Ese módulo no existe, porfavor intente de nuevo')
+        return
+
+    #formato de dia es incorrecto
+    try:
+        int(dia[0])
+        int(dia[1])
+    except:
+        await ctx.send('Tu formato de dia es incorrecto, porfavor intenta de nuevo')
+        return
+
+    #formato de mes es incorrecto
+    if(not(dia[2:]=='marzo' or dia[2:]=='abril' or dia[2:]=='mayo')):
+        await ctx.send('Tu formato de mes es incorrecto, porfavor intenta de nuevo')
+        return
+
+    #El codigo ya existe
+    if(dia in magIA[modulo]):
+        await ctx.send('Este código ya fue almacenado, para acceder a él usa el comando `$getCode`')
+        return
+
+    #almacenar código
     magIA[modulo][dia] = codigo
     with open('./magIAObject.json', 'w') as myObject:
         json.dump(magIA, myObject)
+    await ctx.send('El código de {} {} ha sido salvado correctamente'.format(modulo, dia))
+    return
+
+
 
 
 
