42de0ac79962bf41ba41c07465af6255f7c4902e
Alonso <alonso12_284@outlook.com>
2021-03-19 16:42:19 -0600

    update before deployment

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ from discord.ext import commands
 import json
 
 # Set up logging
-logger = logging.getLogger('discord')
+""" logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(
     filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter(
     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+logger.addHandler(handler) """
 
 # setup intents
 intents = discord.Intents.default()
